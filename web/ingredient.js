65d177610b9fc3983a554e7586d8050ed1143ed8
Van Hau <hauhovnn@gmail.com>
2024-04-17 19:19:57 +0700

    sap xep folders

--- web/ingredient.js
+++ web/ingredient.js
@@ -3,19 +3,19 @@ export function getIngredients() {
     fetch('ingredient_api.php', {
         method: 'GET'
     })
-    .then(response => response.json())
-    .then(data => {
-        // Xử lý dữ liệu được trả về
-        console.log('Ingredients:', data);
-    })
-    .catch(error => {
-        // Xử lý lỗi
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            // Xử lý dữ liệu được trả về
+            console.log('Ingredients:', data);
+        })
+        .catch(error => {
+            // Xử lý lỗi
+            console.error('Error:', error);
+        });
 }
 
 // Gửi yêu cầu POST để tạo mới một nguyên liệu
-export function createIngredient(name, price, quantity, unit) {
+export async function createIngredient(name, price, quantity, unit) {
     const newData = {
         name: name,
         price: price,
@@ -25,25 +25,25 @@ export function createIngredient(name, price, quantity, unit) {
 
     const options = {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({...newData})
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ ...newData })
     };
 
-    fetch('http://127.0.0.1:8080/ingredient_api.php', options)
-    .then(response => response.json())
-    .then(data => {
-        // Xử lý dữ liệu được trả về
-        console.log('Response:', data);
-    })
-    .catch(error => {
-        // Xử lý lỗi
-        console.error('Error:', error);
-    });
+    await fetch('http://127.0.0.1:80/ingredient_api.php', options)
+        .then(response => response.json())
+        .then(data => {
+            // Xử lý dữ liệu được trả về
+            console.log('Response:', data);
+        })
+        .catch(error => {
+            // Xử lý lỗi
+            console.error('Error:', error);
+        });
 }
 
 async function createIngredient2(name, price, quantity, unit) {
     let myObject = await fetch(file);
     let myText = await myObject.text();
     myDisplay(myText);
-  }
+}
 
