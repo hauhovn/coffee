417dc874d8fd3c986ab7d0831e8a442c010bc14b
vhclc <2270125@mail.vnclc.vn>
2024-04-26 13:07:58 +0700

    MAIN>SECTION TINY CHANGE

---
+++ js/menu.js
@@ -0,0 +1,55 @@
+export const MENU = [
+    {
+        gr_id: "CONGVIEC_ID",
+        group_name: "Công việc",
+        items: [
+            {
+                item_id: "GOIMON_ID",
+                link: "#",
+                label: "Gọi món",
+            },
+            {
+                item_id: "THANHTOAN_ID",
+                link: "#",
+                label: "Thanh toán",
+            },
+        ]
+    },
+    {
+        gr_id: "NHANSU_ID",
+        group_name: "Đơn nhân sự",
+        items: [
+            {
+                item_id: "TANGCA_ID",
+                link: "#",
+                label: "Tăng ca",
+            },
+            {
+                item_id: "LICHTRUCCA_ID",
+                link: "#",
+                label: "Lịch trực ca",
+            },
+            {
+                item_id: "NGHIPHEP_ID",
+                link: "#",
+                label: "Nghỉ phép",
+            },
+        ]
+    },
+    {
+        gr_id: "THONGBAO_ID",
+        group_name: "Thông báo",
+        items: [
+            {
+                item_id: "THONGBAOCHUNG_ID",
+                link: "#",
+                label: "Thông báo chung",
+            },
+            {
+                item_id: "BIENDONG_ID",
+                link: "#",
+                label: "Biến động",
+            },
+        ]
+    }
+]
